e10e5f812b8b9699a0e3e85561dd4d2447113ad7
Halalisani <halalisani@iamsignific.com>
2023-06-14 11:12:56 +0200

    fix: slider style fixes

--- src/components/common/sliderTips/sliderData/index.ts
+++ src/components/common/sliderTips/sliderData/index.ts
@@ -12,10 +12,11 @@ const sliderData: ISliderTypes[] = [
   },
   {
     id: 2,
-    // title: 'Rainfal',
-    // description: 'Unlock premium content & features',
-    url: 'https://cdn.pixabay.com/photo/2014/11/21/15/39/grey-crowned-cranes-540657_1280.jpg',
-    // leftIcon:
+    title: 'Sunsets',
+    description: 'Unlock premium features',
+    url: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_960_720.jpg',
+    // url: 'https://cdn.pixabay.com/photo/2014/11/21/15/39/grey-crowned-cranes-540657_1280.jpg',
+    leftIcon: 'weather-sunset-down',
     // actionButtonText: 'Start FREE Trial',
     // url: 'https://cdn.pixabay.com/photo/2018/01/28/13/24/portrait-3113651_1280.jpg',
   },
@@ -25,8 +26,8 @@ const sliderData: ISliderTypes[] = [
     description: 'Unlock premium features NOW...!!!',
     url: 'https://cdn.pixabay.com/photo/2017/06/15/08/11/green-2404444_1280.jpg',
     // url: 'https://cdn.pixabay.com/photo/2017/11/07/00/07/fantasy-2925250_1280.jpg',
-    actionButtonText: 'Start FREE Trial',
-    leftIcon: 'air-filter',
+    actionButtonText: '14-day Free Trial',
+    // leftIcon: 'air-filter',
   },
 ];
 
