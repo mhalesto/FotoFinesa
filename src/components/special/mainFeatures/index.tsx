6ba6a1993ff7a844dcc21893faa9d15d694e9a54
Halalisani <halalisani@iamsignific.com>
2023-06-14 19:43:57 +0200

    fix: main features styling

--- src/components/special/mainFeatures/index.tsx
+++ src/components/special/mainFeatures/index.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {MAINFEATURESDATA} from './main-features-data';
 import CustomAvatar from '../../common/customAvatar';
-import {ICustomAvatarProps} from '../../common/customAvatar/types';
 import {useAppTheme} from '../../../../App';
+import {} from 'react-native-paper';
+import CustomText from '../../common/customText';
+import {IMainFeaturesDataProps} from './main-features-data/types';
 
 const MainFeatures = () => {
   const {
@@ -18,13 +20,12 @@ const MainFeatures = () => {
           [
             {
               backgroundColor: customBodyDark,
-              borderWidth: 0.5,
-              borderBottomColor: '#4c4c4c85',
+              // borderWidth: 0.5,
+              // borderBottomColor: '#4c4c4c85',
             },
           ],
         ]}>
         {MAINFEATURESDATA.map((iconz: any, index: number) => {
-          console.log(iconz);
           const {
             iconName,
             avatarType,
@@ -36,22 +37,39 @@ const MainFeatures = () => {
             label,
             size,
             color,
-          }: ICustomAvatarProps = iconz;
+            featureName,
+            featureNameColor,
+          }: IMainFeaturesDataProps = iconz;
           return (
-            <CustomAvatar
+            <View
               key={index + 1}
-              iconName={iconName}
-              avatarType={avatarType}
-              avatarImagesource={avatarImagesource}
-              avatarPosition={avatarPosition}
-              backgroundColor={backgroundColor}
-              icon={icon}
-              source={source}
-              label={label}
-              size={size}
-              style={{marginHorizontal: 5, borderRadius: 5}}
-              color={color ? color : customBrightGray}
-            />
+              style={{
+                justifyContent: 'center',
+                alignContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+              }}>
+              <CustomAvatar
+                key={index + 1}
+                iconName={iconName}
+                avatarType={avatarType}
+                avatarImagesource={avatarImagesource}
+                avatarPosition={avatarPosition}
+                backgroundColor={backgroundColor}
+                icon={icon}
+                source={source}
+                label={label}
+                size={size}
+                style={{marginHorizontal: 5, borderRadius: 3, opacity: 0.88}}
+                color={color ? color : customBrightGray}
+              />
+              <CustomText
+                text={featureName}
+                style={{padding: 5}}
+                fontSize={13}
+                color={featureNameColor ? featureNameColor : customBrightGray}
+              />
+            </View>
           );
         })}
       </View>
@@ -68,5 +86,19 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     minHeight: 100,
     justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    paddingTop: 15,
+  },
+  shadow: {
+    // borderBottomColor: '#eeeeeeb8',
+    // shadowColor: '#ffffffb8',
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 10,
+    // },
+    // shadowOpacity: 0.51,
+    // shadowRadius: 13.16,
+    // elevation: 24,
   },
 });
